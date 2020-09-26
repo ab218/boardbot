fb5ee439ae59efd6b85e089f5507234abcd0c73b
A B <ab218@live.com>
2020-09-26 14:25:23 -0700

    works

--- index.js
+++ index.js
@@ -1,84 +1,103 @@
 require('dotenv').config();
-const puppeteer = require('puppeteer');
-const schedule = require('node-schedule');
-const moment = require('moment-timezone');
+// const puppeteer = require('puppeteer');
 const Discord = require('discord.js');
+const { MongoClient } = require('mongodb');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const schedule = require('node-schedule');
 const client = new Discord.Client();
-const carnageBoard = 'http://boards.nexustk.com/Carnage/index.html';
+const cotwBoard = 'http://boards.nexustk.com/Chronicles/index.html';
+const channelID = '759519648709345300';
 
-async function run(msg) {
+schedule.scheduleJob('*/5 * * * *', async function() {
+	console.log('running at: ' + Date.now());
+	const newPosts = await getPosts();
+	console.log('newPosts', newPosts);
+	await sendPosts(newPosts);
+});
+
+async function updatePostNumber(client, postno) {
+	await client.db('cotw').collection('postno').updateOne({ _id: 'postno' }, { $set: { postno } }, { upsert: true });
+}
+
+async function getPostNumber(client) {
+	return await client.db('cotw').collection('postno').findOne({ _id: 'postno' }, { _id: 0 });
+}
+
+async function setCotw(targetPostNumber) {
+	const mongoClient = new MongoClient(process.env.MONGO_URL);
 	try {
-		console.log('start');
-		const browser = await puppeteer.launch({ headless: true, args: [ '--no-sandbox', '--disable-setuid-sandbox' ] });
-		const page = await browser.newPage();
-		await page.goto(carnageBoard, { waitUntil: 'networkidle0' });
-		console.log('go to page');
-		await page.waitFor('td');
-		const [ schedulePost ] = await page.$x("//a[contains(., 'hedule')]");
-		if (schedulePost) {
-			await schedulePost.click();
-		} else {
-			return msg.reply('Could not find any schedule... :(');
-		}
-		await page.waitFor('td');
-		const data = await page.$$eval('table tr td', (tds) =>
-			tds.map((td) => {
-				return td.innerText;
-			}),
-		);
-		await browser.close();
-		const dailySchedule = data[8].split('<b>');
-		// schedule jobs
-		const msgChannel = msg.channel.id;
-		dailySchedule.forEach((hour) => {
-			const split = hour.split('\n').filter(Boolean);
-			if (split.length && moment(split[0]).isValid()) {
-				split.forEach((slot) => {
-					const date = moment(split[0]).year(2020).tz('America/New_York');
-					const words = slot.split(' ');
-					const firstWord = words[0].toLowerCase();
-					if (firstWord.includes('am')) {
-						const hour = Number(firstWord.split('am')[0]);
-						const time = date.hour(hour).subtract(30, 'm');
-						const event = words.slice(1).join(' ');
-						const timeChannelID = time.unix() + msgChannel;
-						if (!schedule.scheduledJobs[timeChannelID]) {
-							schedule.scheduleJob(timeChannelID, time.unix(), () => {
-								return client.channels.cache.get(msgChannel).send(`@pk ${event} beginning in 30 minutes!`);
-							});
-						}
-					} else if (firstWord.includes('pm')) {
-						const hour = Number(firstWord.split('pm')[0]);
-						const time = date.hour(hour).add(12, 'h').subtract(30, 'm');
-						const event = words.slice(1).join(' ');
-						const timeChannelID = time.unix() + msgChannel;
-						if (!schedule.scheduledJobs[timeChannelID]) {
-							schedule.scheduleJob(timeChannelID, time.unix(), () => {
-								return client.channels.cache.get(msgChannel).send(`@pk ${event} beginning in 30 minutes!`);
-							});
-						}
-					} else if (firstWord.includes('midn')) {
-						const time = date.hour(23).add(30, 'm');
-						const event = words.slice(1).join(' ');
-						const timeChannelID = time.unix() + msgChannel;
-						if (!schedule.scheduledJobs[timeChannelID]) {
-							schedule.scheduleJob(timeChannelID, time.unix(), () => {
-								return client.channels.cache.get(msgChannel).send(`@pk ${event} beginning in 30 minutes!`);
-							});
-						}
-					}
-				});
-			}
-		});
-		msg.reply('```md\n' + dailySchedule.join('# ').split('**').join('') + '```', {
-			split: {
-				prepend: '```md\n',
-				append: '```',
-			},
-		});
-		return;
+		console.log('setting post number');
+		await mongoClient.connect();
+		await updatePostNumber(mongoClient, Number(targetPostNumber));
 	} catch (e) {
 		console.log(e);
+	} finally {
+		await mongoClient.close();
+	}
+}
+
+async function sendPosts(newPosts) {
+	try {
+		for (i = 0; i < newPosts.length; i++) {
+			const post = newPosts[i];
+			const data = await axios.get(post);
+			const $ = cheerio.load(data.data);
+			const author = $('tr:contains("Author :") td').eq(1).text();
+			const subject = $('tr:contains("Subject :") td').eq(1).text();
+			const date = $('tr:contains("Date :") td').eq(1).text();
+			const body = $('tr:nth-child(5) td').text();
+			client.channels.cache
+				.get(channelID)
+				.send(
+					'```\n' +
+						'Author: ' +
+						author +
+						'\nSubject: ' +
+						subject +
+						'\nDate: ' +
+						date +
+						'\nBody: \n' +
+						body.split('**').join('') +
+						'```',
+					{
+						split: {
+							prepend: '```\n',
+							append: '```',
+						},
+					},
+				);
+		}
+	} catch (e) {
+		console.log(e);
+	}
+}
+
+async function getPosts() {
+	const mongoClient = new MongoClient(process.env.MONGO_URL);
+	try {
+		console.log('start');
+		await mongoClient.connect();
+		const data = await axios.get(cotwBoard);
+		const $ = cheerio.load(data.data);
+		const { postno } = await getPostNumber(mongoClient);
+		let topPost = postno;
+		const links = [];
+		$('tr td:first-child a').each(function() {
+			const postNumber = Number($(this).text());
+			if (postNumber > topPost) {
+				topPost = postNumber;
+			}
+			if (postNumber > postno) {
+				links.push(`http://boards.nexustk.com/Chronicles/${$(this).attr('href')}`);
+			}
+		});
+		await updatePostNumber(mongoClient, topPost);
+		return links;
+	} catch (e) {
+		console.log(e);
+	} finally {
+		await mongoClient.close();
 	}
 }
 
@@ -87,11 +106,16 @@ client.on('ready', () => {
 });
 client.on('message', async (msg) => {
 	try {
-		if (msg.channel.name === 'carnage-schedule' && msg.content === '!schedule') {
-			run(msg);
+		const message = msg.content.split(' ');
+		if (message[0] === '!cotw') {
+			const newPosts = await getPosts();
+			console.log('newPosts', newPosts);
+			await sendPosts(newPosts);
+		} else if (message[0] === '!cotwset') {
+			setCotw(message[1]);
 		}
 	} catch (e) {
 		console.log(e);
 	}
 });
-client.login(process.env.CARNY_BOT_TOKEN);
+client.login(process.env.COTW_BOT_TOKEN);
